0c93253c4d972c1bb4e9c0a6e8971a03fb7fd171
Safonov <softik.d@gmail.com>
2023-10-30 14:17:26 +0300

    choose Autenticak Transport & Strategy

---
+++ src/auth/auth.py
@@ -0,0 +1,18 @@
+from fastapi_users.authentication import CookieTransport, AuthenticationBackend
+from fastapi_users.authentication import JWTStrategy
+
+from config import SECRET_AUTH
+
+cookie_transport = CookieTransport(cookie_name="rabbitmg", cookie_max_age=3600)
+SECRET = SECRET_AUTH
+
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=cookie_transport,
+    get_strategy=get_jwt_strategy,
+)
