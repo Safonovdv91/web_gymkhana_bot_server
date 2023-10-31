773066b77ab976e9072d6ae9752f919692eecb8a
Safonov <softik.d@gmail.com>
2023-10-31 09:28:24 +0300

    add manager.py

--- config.py
+++ config.py
@@ -20,4 +20,5 @@ REDIS_HOST = os.environ.get("REDIS_HOST")
 REDIS_PORT = os.environ.get("REDIS_PORT")
 
 SECRET_AUTH = os.environ.get("SECRET_AUTH")
+SECRET_AUTH_MANAGER = os.environ.get("SECRET_AUTH_MANAGER")
 
