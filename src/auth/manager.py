2af33e9280a2b15768d270c97e533c7304d00f12
Safonov <softik.d@gmail.com>
2023-11-01 15:15:00 +0300

    refactoring

--- src/auth/manager.py
+++ src/auth/manager.py
@@ -1,7 +1,13 @@
 from typing import Optional
 
 from fastapi import Depends, Request
-from fastapi_users import BaseUserManager, IntegerIDMixin, exceptions, schemas, models
+from fastapi_users import (
+    BaseUserManager,
+    IntegerIDMixin,
+    exceptions,
+    models,
+    schemas,
+)
 
 from config import SECRET_AUTH_MANAGER
 from src.auth.utils import get_user_db
