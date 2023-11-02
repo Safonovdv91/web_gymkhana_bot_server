ae85f24eba72491372843d3831f7b283025751c1
Safonov <softik.d@gmail.com>
2023-11-02 08:46:03 +0300

    bug fix models.py & revision

--- src/auth/models.py
+++ src/auth/models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from typing import Optional
 
 from fastapi_users.db import SQLAlchemyBaseUserTable
-from sqlalchemy import Boolean, MetaData
+from sqlalchemy import Boolean, MetaData, false
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -15,37 +15,46 @@ metadata = MetaData()
 class User(SQLAlchemyBaseUserTable[int], Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    hashed_password: Mapped[str] = mapped_column(nullable=False)
+    id: Mapped[int] = mapped_column(
+        primary_key=True
+    )
+    hashed_password: Mapped[str] = mapped_column(
+        nullable=False
+    )
     ggp_percent_begin: Mapped[int] = mapped_column(
         default=100,
     )
-    ggp_percent_end: Mapped[int] = mapped_column(default=150)
-
-    sub_ggp_percent: Mapped[int] = mapped_column(default=False)
-    sub_offline: Mapped[bool] = mapped_column(default=False)
-    sub_ggp: Mapped[bool] = mapped_column(default=False)
-    sub_world_record: Mapped[bool] = mapped_column(default=False)
+    ggp_percent_end: Mapped[int] = mapped_column(
+        default=150
+    )
+    sub_ggp_percent: Mapped[bool] = mapped_column(
+        default=False
+    )
+    sub_offline: Mapped[bool] = mapped_column(
+        default=False
+    )
+    sub_ggp: Mapped[bool] = mapped_column(
+        default=False
+    )
+    sub_world_record: Mapped[bool] = mapped_column(
+        Boolean, default=False, nullable=False, server_default=false()
+    )
     telegram_id: Mapped[Optional[int]]
     login: Mapped[Optional[str]]
-    email: Mapped[Optional[str]] = mapped_column(unique=True)
-    registered_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
-    is_active: Mapped[bool] = mapped_column(
-        Boolean,
-        default=True,
+    email: Mapped[Optional[str]] = mapped_column(
+        unique=True
     )
-    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False)
-    is_verified: Mapped[bool] = mapped_column(Boolean, default=False)
-
-    is_active: Mapped[bool] = mapped_column(
-        Boolean,
-        default=True,
+    registered_at: Mapped[datetime] = mapped_column(
+        default=datetime.utcnow
     )
-    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False)
-    is_verified: Mapped[bool] = mapped_column(Boolean, default=False)
-
-    is_pidor: Mapped[bool] = mapped_column(
-        Boolean, default=False, nullable=False
+    is_active: Mapped[bool] = mapped_column(
+        Boolean, default=True,
+    )
+    is_superuser: Mapped[bool] = mapped_column(
+        Boolean, default=False, nullable=False, server_default=false()
+    )
+    is_verified: Mapped[bool] = mapped_column(
+        Boolean, default=False, nullable=False, server_default=false()
     )
 
 
