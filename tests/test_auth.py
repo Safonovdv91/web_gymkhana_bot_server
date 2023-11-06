d37f4341c48fe497b8d6e1547a679d7c3faaaed8
Dmitry Safonov <softik.d@gmail.com>
2023-11-06 17:20:25 +0300

    test: /auth/register ok

---
+++ tests/test_auth.py
@@ -0,0 +1,38 @@
+from sqlalchemy import insert, select
+
+from src.users.models import Role
+from tests.conftest import client, async_session_maker
+
+
+async def test_add_role():
+    async with async_session_maker() as session:
+        stmt = insert(Role).values(id=1, name="test_admin", description="Test of admin")
+        await session.execute(stmt)
+        await session.commit()
+
+        query = select(Role)
+        result = await session.scalar(query)
+
+        assert result.id == 1, "Проблема с добавлением роли"
+        assert result.name == "test_admin"
+        assert result.description == "Test of admin"
+
+        stmt = insert(Role).values(id=2, name="test_user", description="Test of user")
+        await session.execute(stmt)
+        await session.commit()
+
+
+def test_register():
+    response = client.post(
+        url="/auth/register",
+        json={
+            "email": "user3@example.com",
+            "password": "string",
+            "is_active": True,
+            "is_superuser": False,
+            "is_verified": False,
+            "login": "string"
+        }
+    )
+
+    assert response.status_code == 201, "User not add"
