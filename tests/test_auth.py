ed42649b4cdc3001f003b4ef89e48a0fa3faf172
Dmitry Safonov <softik.d@gmail.com>
2023-11-09 13:27:06 +0300

    testing api get + validation

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,72 +1,69 @@
 from httpx import AsyncClient
 from sqlalchemy import insert, select
-
 from src.users.models import Role
 from tests.conftest import client, async_session_maker
 
 
-async def test_add_role():
-    async with async_session_maker() as session:
-        stmt = insert(Role).values(id=1, name="test_admin", description="Test of admin")
-        await session.execute(stmt)
-        await session.commit()
+class TestApiAuthentification:
 
-        query = select(Role)
-        result = await session.scalar(query)
+    async def test_add_role(self, ):
+        async with async_session_maker() as session:
+            stmt = insert(Role).values(id=1, name="test_admin", description="Test of admin")
+            await session.execute(stmt)
+            await session.commit()
 
-        assert result.id == 1, "Проблема с добавлением роли"
-        assert result.name == "test_admin"
-        assert result.description == "Test of admin"
+            query = select(Role)
+            result = await session.scalar(query)
 
-        stmt = insert(Role).values(id=2, name="test_user", description="Test of user")
-        await session.execute(stmt)
-        await session.commit()
+            assert result.id == 1, "Проблема с добавлением роли"
+            assert result.name == "test_admin"
+            assert result.description == "Test of admin"
 
+            stmt = insert(Role).values(id=2, name="test_user", description="Test of user")
+            await session.execute(stmt)
+            await session.commit()
 
-def test_register_post():
-    response = client.post(
-        url="/auth/register",
-        json={
-            "email": "user3@example.com",
-            "password": "string",
-            "is_active": True,
-            "is_superuser": False,
-            "is_verified": False,
-            "login": "string"
-        }
-    )
+    def test_register_post(self, ):
+        response = client.post(
+            url="/auth/register",
+            json={
+                "email": "user3@example.com",
+                "password": "string",
+                "is_active": True,
+                "is_superuser": False,
+                "is_verified": False,
+                "login": "string"
+            }
+        )
 
-    assert response.status_code == 201, "User not add"
+        assert response.status_code == 201, "User not add"
 
+    def test_register_post_duplicate(self, ):
+        response = client.post(
+            url="/auth/register",
+            json={
+                "email": "user3@example.com",
+                "password": "string",
+                "is_active": True,
+                "is_superuser": False,
+                "is_verified": False,
+                "login": "string"
+            }
+        )
 
-def test_register_post_duplicate():
-    response = client.post(
-        url="/auth/register",
-        json={
-            "email": "user3@example.com",
-            "password": "string",
-            "is_active": True,
-            "is_superuser": False,
-            "is_verified": False,
-            "login": "string"
-        }
-    )
+        assert response.status_code == 400, "Duplicat was added"
 
-    assert response.status_code == 400, "Duplicat was added"
+    def test_register_post_not_all_data(self, ):
+        response = client.post(
+            url="/auth/register",
+            json={
+                "email": "user_valid@example.com",
+                "password": "string",
+                "login": "Loh"
+            }
+        )
+        assert response.status_code == 201, "Duplicat was added"
 
-
-def test_register_post_not_all_data():
-    response = client.post(
-        url="/auth/register",
-        json={
-            "email": "user_valid@example.com",
-            "password": "string",
-            "login": "Loh"
-        }
-    )
-    assert response.status_code == 201, "Duplicat was added"
-
-
-async def test_register_get_not_all_data(ac: AsyncClient):
-    response = await ac.get("/users/2")
-    assert response.json()["data"]["email"] == "user_valid@example.com", "User is not valid"
+    async def test_register_get_not_all_data(self, ac: AsyncClient):
+        response = await ac.get("/api/v1/users/2")
+        assert response.json()["data"]["email"] == "user_valid@example.com", "User is not valid"
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,43 +1,60 @@
+import pytest
 from httpx import AsyncClient
 
 
-async def test_register_post(ac: AsyncClient):
-    response = await ac.post("/auth/register", json={
-        "email": "user_for_delete@example.com",
-        "password": "string",
-        "is_active": True,
-        "is_superuser": False,
-        "is_verified": False,
-        "login": "string"
-    })
-    assert response.status_code == 201, "User add register"
+class TestApiUser:
+
+    async def test_register_post(self, ac: AsyncClient):
+        response = await ac.post("/auth/register", json={
+            "email": "user_for_delete@example.com",
+            "password": "string",
+            "is_active": True,
+            "is_superuser": False,
+            "is_verified": False,
+            "login": "string"
+        })
+        assert response.status_code == 201, "User add register"
+
+    # async def test_user_get_unauthorized(ac: AsyncClient):
+    #     response = await ac.get("/api/v1/users/get", )
+    #     assert response.status_code == 401, "UnAuthorized "
+
+    # @pytest.fixture
+    # async def login_cookies(ac: AsyncClient):
+    #     url = "/auth/jwt/login"
+    #     data = {
+    #         "username": "user3@example.com",
+    #         "password": "string",
+    #     }
+    #     response = await ac.post(url=url, data=data)
+    #     return response.cookies
 
 
-async def test_user_get_unauthorized(ac: AsyncClient):
-    response = await ac.get("/users/get", )
-    assert response.status_code == 401, "UnAuthorized "
+class TestUserApi:
+    common_url = "/api/v1/users"
 
+    emails = [
+        ("user3@example.com"),
+        ("user1@example.com"),
+        ("@@@"),
+        ("123"),
+    ]
 
-# @pytest.fixture
-# async def login_cookies(ac: AsyncClient):
-#     url = "/auth/jwt/login"
-#     data = {
-#         "username": "user3@example.com",
-#         "password": "string",
-#     }
-#     response = await ac.post(url=url, data=data)
-#     return response.cookies
-
-
-class TestUser:
-    async def test_user_get_by_id_ok(self, ac: AsyncClient):
-        url = "/users/get/email=user3@example.com"
+    @pytest.mark.parametrize('email', emails)
+    async def test_user_get_by_id_ok(self, ac: AsyncClient, email):
+        # url = f"{self.common_url}/get/email=user3@example.com"
+        url = f"{self.common_url}/get/email={email}"
         response = await ac.get(url=url)
-        assert response.status_code == 200
-        assert response.json()["data"]["email"] == "user3@example.com"
+        if email == "user3@example.com":
+            assert response.status_code == 200
+            assert response.json()["data"]["email"] == "user3@example.com"
+        elif email in ("@@@", "123"):
+            assert response.status_code == 422
+        else:
+            assert response.status_code == 404
 
     async def test_user_get_by_id_(self, ac: AsyncClient):
-        url = "/users/get/email=usernotexist@example.com"
+        url = f"{self.common_url}/get/email=usernotexist@example.com"
         response = await ac.get(url=url)
         assert response.status_code == 404, "Not exist user -> not 404"
 
