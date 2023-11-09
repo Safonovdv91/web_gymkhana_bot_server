87414df0fc5d2fcbdb3c104638a1456f69c0d7be
Dmitry Safonov <softik.d@gmail.com>
2023-11-09 09:13:08 +0300

    Deploy success

--- app.sh
+++ app.sh
@@ -2,7 +2,7 @@
 
 echo "my location is"
 echo "Накатываем миграции в БД"
-alembic -c alembic.ini upgrade head
+alembic upgrade head
 echo "******"
 echo "******"
 gunicorn src.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
