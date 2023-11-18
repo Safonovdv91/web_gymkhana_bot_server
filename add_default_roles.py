eef655a5f933b7493533f3cf4c06c26d0555c7b5
Dmitry Safonov <softik.d@gmail.com>
2023-11-18 09:51:13 +0300

    reformat

--- add_default_roles.py
+++ add_default_roles.py
@@ -4,15 +4,32 @@ from src.database import engine
 
 print("Добавляем стандартные роли")
 
+
 async def add_roles():
     async with engine.begin() as conn:
         try:
-            await conn.execute(text("INSERT INTO roles VALUES(1, 'Super_User','Can change role all users/admins, "
-                                    "delete or create')"))
-            await conn.execute(text("INSERT INTO roles VALUES(2, 'Admin','Administrator can del users, and change user's "
-                                    "settings')"))
-            await conn.execute(text("INSERT INTO roles VALUES(3, 'User','Can only change himself settings')"))
-            await conn.execute(text("INSERT INTO roles VALUES(4, 'Guest','Unknown person, can nothing')"))
+            await conn.execute(
+                text(
+                    "INSERT INTO roles VALUES(1, 'Super_User','Can change role all users/admins, "
+                    "delete or create')"
+                )
+            )
+            await conn.execute(
+                text(
+                    "INSERT INTO roles VALUES(2, 'Admin','Administrator can del users, and change user's "
+                    "settings')"
+                )
+            )
+            await conn.execute(
+                text(
+                    "INSERT INTO roles VALUES(3, 'User','Can only change himself settings')"
+                )
+            )
+            await conn.execute(
+                text(
+                    "INSERT INTO roles VALUES(4, 'Guest','Unknown person, can nothing')"
+                )
+            )
         except Exception as e:
             print("<----Добавление пошло не по плану!---->")
             print(f"ERROR: {e}")
