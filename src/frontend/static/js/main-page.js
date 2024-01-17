dd595e80122b0e534e87d2129b0240a17ff08163
pavelnov67 <104889944+pavelnov67@users.noreply.github.com>
2024-01-17 21:01:24 +0300

    update files
    замена аутх на логин
    и туда же адаптив

--- src/frontend/static/js/main-page.js
+++ src/frontend/static/js/main-page.js
@@ -1,64 +1,12 @@
 
 
-const requestURL = 'http://127.0.0.1:9000/api/v1/users';
-
-// блок кода для выпадающих меню
-function myFunction() {
-  document.getElementById("myDropDown").classList.toggle("show");
-}
-
-function myFunction2() {
-  document.getElementById("myDropDown2").classList.toggle("show");
-}
-
-function myFunction3() {
-  document.getElementById("myDropDown3").classList.toggle("show");
-}
-
-// Закройте выпадающее меню, если пользователь щелкает за его пределами
-window.onclick = function(event) {
-  if (!event.target.matches('.hi__listDropBth')) {
-    var dropdowns = document.getElementsByClassName("drop__content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  } else if (!event.target.matches('.hi__listDropBth2')) {
-    var dropdowns = document.getElementsByClassName("drop__content2");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  } else if (!event.target.matches('.hi__listDropBth3')) {
-    var dropdowns = document.getElementsByClassName("drop__content3");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  } else (alert("всё поломалось"))
-}
-
-
-
-fetch('http://127.0.0.1:9000/api/v1/users/current',  {
-  method: 'GET',
-  credentials: 'include',
-})
-      .then(response => {
-        console.log(response)
-        return response.json()
+let response = fetch('http://127.0.0.1:9000/api/v1/users/ALL_INFO',  {
+    method: 'GET',
+    credentials: 'include',
+  })
+    .then(response => {
+      return response.json()
       })
       .then(json => console.log(json))
       .catch(error => console.log(error))
-
-
 
