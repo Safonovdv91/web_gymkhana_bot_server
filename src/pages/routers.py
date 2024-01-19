ff9473f70db8a4b55e430dcbad58e577fa064c63
Safonov <Sofik.d@gmail.com>
2024-01-19 08:01:47 +0300

    add current_user

--- src/pages/routers.py
+++ src/pages/routers.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, Request
 from starlette.templating import Jinja2Templates
 
-from src.users.router import get_user_by_id, get_users
-
+from src.users.router import get_user_by_id, get_users, get_current_user
 
 router = APIRouter(tags=["Frontend"])
 templates = Jinja2Templates(directory="src/frontend/templates")
@@ -12,6 +11,16 @@ templates = Jinja2Templates(directory="src/frontend/templates")
 def get_base_page(request: Request):
     return templates.TemplateResponse("base.html", {"request": request})
 
+
+@router.get("/current_user")
+def get_users_page(
+        request: Request,
+        users=Depends(get_current_user)
+):
+    return templates.TemplateResponse(
+        name="getusers.html",
+        context={"request": request, "users": [users["data"]]}
+    )
 
 @router.get("/get_users")
 def get_users_page(
@@ -37,11 +46,15 @@ def get_users_page_id(request: Request, usr=Depends(get_user_by_id)):
 
 @router.get("/registration")
 def get_register_page(request: Request):
-    return templates.TemplateResponse("registration.html", {"request": request})
+    return templates.TemplateResponse(
+        "registration.html", {"request": request}
+    )
 
 
 @router.get("/login")
 async def get_login_page(
     request: Request,
 ):
-    return templates.TemplateResponse("authorization.html", {"request": request})
+    return templates.TemplateResponse(
+        "authorization.html", {"request": request}
+    )
