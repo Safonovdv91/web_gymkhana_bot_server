f849096bf64703478a3afe36c5496450cca8d016
Safonov <Sofik.d@gmail.com>
2024-02-13 09:34:51 +0300

    refactoring, add fx

--- src/frontend/static/js/getusers.js
+++ src/frontend/static/js/getusers.js
@@ -1,23 +1,33 @@
 
 
-//медот PATCH запроса
+// Метод обновления состояния буковки подписки класса
+function updateSubGGPClasses(user) {
+      let userId = user.id
+      let formUser = document.getElementsByClassName('user');
+      for (let elem of formUser) {                                       // перебор html форм class="user"
+        let formId = elem.getAttribute('data-userid')
+        if (userId == formId) {
+          let ggpClasses = elem.getElementsByClassName('ggp-classes-sub');
+          for (let elem of ggpClasses) {
+            user.ggp_sub_classes.forEach(classLetter => {
+              if (elem.textContent == classLetter.sport_class) {
+                elem.classList.add('active')
+              }
+            })
+          }
+        }
+      }
+    }
 
-(function asd() {
-
-  function onSubmit(event) {
-
-    event.preventDefault();
-    console.log('Отправка!')
-
+function patchSubGGPClasses(event,operation,literal) {
+    console.log("Begin")
     let button = event.target;
-
     const userid = button.dataset.userid;
-
     fetch(`${AppConsts.BaseUrl}/api/v1/users/id=${userid}/subscribe`, {
       method: 'PATCH',
       body: JSON.stringify({
-        op: "add",
-        sport_class: "C1",
+        op: operation,
+        sport_class: literal,
       }),
       headers: {
         'Content-Type': 'application/json; charset=UTF-8'
@@ -25,6 +35,26 @@
     })
       .then(response => response.json())
       .then(json => console.log(json.details))
+
+
+
+
+}
+
+  function onSubmitGGPClasses(event) {
+      patchSubGGPClasses(event,"add","A");
+  }
+
+
+//медот PATCH запроса
+(function asd() {
+
+  function onSubmit(event) {
+
+    event.preventDefault();
+    console.log('Отправка!')
+
+    patchSubGGPClasses(event,"remove","A");
   }
 
   let elements = document.getElementsByClassName('patch-button-submit');
@@ -34,6 +64,45 @@
   }
 })();
 
+
+//(function sub_A() {
+//
+//  function onSubmit(event) {
+//      patchSubGGPClasses(event,"add","A");
+//  }
+//  let subA = document.getElementsByClassName('button ggp-classes-sub A-class-button-submit');
+//  subA[0].addEventListener('click', onSubmit);
+//})();
+
+(function sub_B() {
+
+  function onSubmit(event) {
+
+    event.preventDefault();
+    let button = event.target;
+
+    const userid = button.dataset.userid;
+
+    fetch(`${AppConsts.BaseUrl}/api/v1/users/id=${userid}/subscribe`, {
+      method: 'PATCH',
+      body: JSON.stringify({
+        op: "add",
+        sport_class: "B",
+      }),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8'
+      }
+    })
+      .then(response => response.json())
+      .then(json => console.log(json.details))
+  }
+
+  let elements = document.getElementsByClassName('button ggp-classes-sub B-class-button-submit');
+
+  for (let element of elements) {
+    element.addEventListener('click', onSubmit);
+  }
+})();
 
 // метод DELETE запроса
 
@@ -65,16 +134,15 @@
 
 
 
-
-const elements = document.getElementsByClassName('ggp-classes-sub')
-
-function handleClickFunction(event) {
-  event.target.classList.toggle('active'); 
-}
-
-for (let element of elements) {
-element.addEventListener('click', handleClickFunction)
-}
+//// работа с кнопками классов
+//let elements = document.getElementsByClassName('ggp-classes-sub');
+//const cons = () => {
+//  elements.classList.add('active')
+//}
+//
+//for (let element of elements) {
+//  element.addEventListener('click', cons);
+//}
 
 
 fetch(`${AppConsts.BaseUrl}/api/v1/users`, {
@@ -82,36 +150,18 @@ fetch(`${AppConsts.BaseUrl}/api/v1/users`, {
   credentials: 'include',
 })
   .then(response => {
-
     return response.json()
   })
   .then(json => {
-
-    json.data.forEach(user => {
-      let userId = user.id
-      let formUser = document.getElementsByClassName('user');
-      for (let elem of formUser) {                                       // перебор html форм class="user"
-        let formId = elem.getAttribute('data-userid')
-        if (userId == formId) {
-
-          let ggpClasses = elem.getElementsByClassName('ggp-classes-sub');
-          for (let elem of ggpClasses) {
-            console.log(elem.textContent)
-
-
-            user.ggp_sub_classes.forEach(classLetter => {
-              console.log(classLetter.sport_class)
-              if (elem.textContent == classLetter.sport_class) {
-                elem.classList.add('active')
-              }
-            })
-          }
-        }
-      }
-    });
-
-
+    json.data.forEach(user => updateSubGGPClasses(user));
   })
   .catch(error => console.log(error))
+
+
+let subA = document.getElementsByClassName('button ggp-classes-sub A-class-button-submit');
+subA[0].addEventListener('click', onSubmitGGPClasses);
+//медот PATCH запроса
+
+
 
 
