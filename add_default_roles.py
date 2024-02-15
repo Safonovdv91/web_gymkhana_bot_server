19ada71a49fdb95761a94b13fb256d585fe01ef1
Safonov <Sofik.d@gmail.com>
2024-02-15 21:31:06 +0300

    add all default sport classes and roles

--- add_default_roles.py
+++ add_default_roles.py
@@ -7,84 +7,75 @@ from logger.logger import logger
 from src.database import engine
 
 
-async def add_roles():
-    logger.info("Adding roles and sport class")
+async def add_sport_class(sport_class, description):
+    logger.info(f"Adding sport_class: {sport_class}| [{description}]")
     async with engine.begin() as conn:
         try:
             await conn.execute(
                 text(
-                    "INSERT INTO roles (name, description) VALUES('Super_User','Can change role all users/admins, "
-                    "delete or create')"
+                    f"INSERT INTO sport_classes (sport_class, description) VALUES('{sport_class}','{description}')"
                 )
             )
-            await conn.execute(
-                text(
-                    "INSERT INTO roles (name, description) VALUES('Admin','Administrator can del users, and change users "
-                    "settings')"
-                )
-            )
-            await conn.execute(
-                text(
-                    "INSERT INTO roles (name, description) VALUES('User','Can only change himself settings')"
-                )
-            )
-            await conn.execute(
-                text(
-                    "INSERT INTO roles (name, description) VALUES('Guest','Unknown person, can nothing')"
-                )
-            )
-            logger.info("Adding roles complete. Adding roles...")
-            await conn.execute(
-                text(
-                    "INSERT INTO sport_classes (sport_class, description) VALUES('A','Best of the best')"
-                )
-            )
-            await conn.execute(
-                text(
-                    "INSERT INTO sport_classes (sport_class, description) VALUES('B','Sportsman speed rate: 100-105%')"
-                )
-            )
-            await conn.execute(
-                text(
-                    "INSERT INTO sport_classes (sport_class, description) VALUES('C1','Sportsman speed rate: 105-110%')"
-                )
-            )
-            await conn.execute(
-                text(
-                    "INSERT INTO sport_classes (sport_class, description) VALUES('C2','Sportsman speed rate: 110-115%')"
-                )
-            )
-            await conn.execute(
-                text(
-                    "INSERT INTO sport_classes (sport_class, description) VALUES('C3','Sportsman speed rate: 115-120%')"
-                )
-            )
-            logger.info("Adding sport classes complete!")
         except (SQLAlchemyError, Exception) as e:
             if isinstance(e, SQLAlchemyError):
                 msg = "Database Exc: "
+                logger.error(msg=msg, exc_info=True)
             else:
                 msg = "Unknown Exc: "
                 msg += "add standard roles and sport-class error!"
                 logger.error(msg=msg, exc_info=True)
 
 
-# async def add_users():
-#     print("Добавляем стандартного юзера")
-#
-#     async with engine.begin() as conn:
-#         try:
-#             await conn.execute(
-#                 text("INSERT INTO users (hashed_password, login, email, role_id, ggp_percent_begin, ggp_percent_end, "
-#                      "sub_ggp_percent, sub_offline, sub_ggp, registered_at, is_active) VALUES ("
-#                      "'$2b$12$bQ/w7vHbxqmuzQIusdwMvOvfhuu2ChGjcU51CLuqZw4cgYiPh8Vs.', 'def_admin@example.com', "
-#                      "'string', 1, 100, 150, True, False, False, '2023-11-10 21:14:43.681332', True);"
-#                 )
-#             )
-#
-#         except Exception as e:
-#             print("<----Добавление User ошибка не по плану!---->")
-#             print(f"ERROR: {e}")
+async def add_role(role_name, description):
+    logger.info(f"Adding role: {role_name}| [{description}]")
+    async with engine.begin() as conn:
+        try:
+            await conn.execute(
+                text(
+                    f"INSERT INTO roles (name, description) VALUES('{role_name}','{description}')"
+                )
+            )
+        except (SQLAlchemyError, Exception) as e:
+            if isinstance(e, SQLAlchemyError):
+                msg = "Database Exc: "
+                logger.error(msg=msg, exc_info=True)
+            else:
+                msg = "Unknown Exc: "
+                msg += "add standard roles and sport-class error!"
+                logger.error(msg=msg, exc_info=True)
 
 
-asyncio.run(add_roles())
+async def add_default_values():
+    logger.info("Adding default values")
+
+    DEFAULT_SPORT_CLASSES = {
+        "A": "Best of the best",
+        "B": "Sportsman speed rate: 100-105%",
+        "C1": "Sportsman speed rate: 105-110%",
+        "C2": "Sportsman speed rate: 110-115%",
+        "C3": "Sportsman speed rate: 115-120%",
+        "D1": "Sportsman speed rate: 120-130%",
+        "D2": "Sportsman speed rate: 130-140%",
+        "D3": "Sportsman speed rate: 140-150%",
+        "D4": "Sportsman speed rate: 150-160%",
+        "N": "Sportsman speed rate: 160+%",
+    }
+
+    DEFAULT_ROLES ={
+        "Owner": "Can change role all users/admins, delete or create",
+        "Admin": "Administrator can del users, and change user's settings",
+        "User": "Can only change himself settings",
+        "Guest": "Unknown person, can nothing",
+    }
+
+    logger.info("Adding roles ...")
+    for k,v in DEFAULT_ROLES.items():
+        await add_role(k, v)
+    logger.info("Adding DEFAULT ROLES complete! success")
+
+    logger.info("Adding sport_classes...")
+    for k,v in DEFAULT_SPORT_CLASSES.items():
+        await add_sport_class(k, v)
+    logger.info("Adding DEFAULT SPORT CLASSES complete! success")
+
+asyncio.run(add_default_values())
