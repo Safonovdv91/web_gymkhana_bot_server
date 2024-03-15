ecf1663c27ccfa6964f32f788e8d1def0be241ea
pavelnov67 <104889944+pavelnov67@users.noreply.github.com>
2024-03-15 10:51:23 +0300

    hide button
    реализовал кнопу для скрытия/раскрытия иконок классов

--- src/frontend/static/js/getusers.js
+++ src/frontend/static/js/getusers.js
@@ -1,5 +1,25 @@
 
 
+// сворачивание кнопок классов
+
+
+let coll = document.getElementsByClassName('collaps')
+
+for (let i = 0; i < coll.length; i++) {
+  coll[i].addEventListener('click', function (event) {
+    event.preventDefault();
+    console.log('click')
+    this.classList.toggle('active');
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + 'px'
+    }
+  })
+}
+
+
 //медот PATCH запроса
 
 (function asd() {
@@ -73,7 +93,7 @@ function handleClickFunction(event) {
 }
 
 for (let element of elements) {
-element.addEventListener('click', handleClickFunction)
+  element.addEventListener('click', handleClickFunction)
 }
 
 
