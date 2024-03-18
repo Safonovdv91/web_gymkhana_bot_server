fbe737d2a82af1e9140f3afb701909657df29d3a
cr1mp <cr1mp@ya.ru>
2024-03-18 22:32:34 +0300

    Refactoring

--- src/frontend/static/js/getusers.js
+++ src/frontend/static/js/getusers.js
@@ -1,210 +1,160 @@
-function updateStatementGGPSubscribing() {
-  console.log('Обновляем данные о GGP')
-  fetch(`${AppConsts.BaseUrl}/api/v1/users`, {
-    method: 'GET',
-    credentials: 'include',
-  })
-    .then(response => {
-      return response.json()
-    })
-    .then(json => {
-      json.data.forEach(user => updateSubGGPClasses(user));
-    })
-    .catch(error => console.log(error))
-}
+var rmg = rmg || {};
 
-// Метод обновления состояния буковки подписки класса
-function updateSubGGPClasses(user) {
-  let userId = user.id
-  let formUser = document.getElementsByClassName('user');
-  for (let elem of formUser) {                                       // перебор html форм class="user"
-    let formId = elem.getAttribute('data-userid')
-    if (userId == formId) {
-      let ggpClasses = elem.getElementsByClassName('ggp-classes-sub');
-      for (let elem of ggpClasses) {
-        user.ggp_sub_classes.forEach(classLetter => {
-          if (elem.textContent == classLetter.sport_class) {
-            elem.classList.add('active')
-          }
-        })
+(function () {
+
+  // Метод обновления состояния буковки подписки класса.
+  function updateSubGGPClasses(user) {
+    let userId = user.id;
+    let formUsers = document.getElementsByClassName('user');
+    // перебор html форм class="user"
+    for (let formUser of formUsers) {
+      let formId = formUser.getAttribute('data-userid');
+      if (userId == formId) {
+        let ggpClasses = formUser.getElementsByClassName('ggp-classes-sub');
+        for (let ggpClass of ggpClasses) {
+          console.log(ggpClass.textContent);
+          user.ggp_sub_classes.forEach(classLetter => {
+            console.log(classLetter.sport_class);
+            if (ggpClass.textContent == classLetter.sport_class) {
+              ggpClass.classList.add('active');
+            }
+          })
+        }
       }
     }
   }
-}
 
-// сворачивание кнопок классов
-
-
-let coll = document.getElementsByClassName('collaps')
-
-for (let i = 0; i < coll.length; i++) {
-  coll[i].addEventListener('click', function (event) {
-    event.preventDefault();
-    console.log('click')
-    this.classList.toggle('active');
-    let content = this.nextElementSibling;
-    if (content.style.maxHeight) {
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + 'px'
-    }
-  })
-}
-
-
-//медот PATCH запроса
-
-(function asd() {
-
-  function onSubmit(event) {
-
-    event.preventDefault();
-    console.log('Отправка!')
-
-    // функция подписки на классы в соответствии с логикой
-    function patchSubGGPClasses(event, operation, literal) {
-
-      let button = event.target;
-      const userid = button.dataset.userid;
-      fetch(`${AppConsts.BaseUrl}/api/v1/users/id=${userid}/subscribe`, {
-        method: 'PATCH',
-        body: JSON.stringify({
-          op: operation,
-          sport_class: literal,
-        }),
-        headers: {
-          'Content-Type': 'application/json; charset=UTF-8'
-        }
-      })
-        .then(response => response.json())
-        .then(json => console.log(json.details))
-    }
+  // Получить пользователей.
+  function getUsers() {
+    console.log('Обновляем данные о GGP');
+    fetch(`${AppConsts.BaseUrl}/api/v1/users`, {
+      method: 'GET',
+      credentials: 'include',
+    }).then(response => {
+      return response.json();
+    }).then(json => {
+      json.data.forEach(user => {
+        updateSubGGPClasses(user);
+      });
+    }).catch(error => console.log(error));
   }
-})
 
-// Переключатель свечения кнопки и выбора метода
-function handleClickFunction(event) {
-  event.target.classList.toggle('active');
-  if (event.target.getAttribute('class').includes('active')) {
-    patchSubGGPClasses(event, 'add', event.target.innerText)
-  } else {
-    patchSubGGPClasses(event, 'remove', event.target.innerText)
-  }
-}
-
-// метод DELETE запроса - это переделать
-(function basd() {
-
-  function onSubmit(event) {
-
+  // Удалить пользователя.
+  function onDelete(event) {
     event.preventDefault();
     console.log('Отправка!')
 
     let button = event.target;
+    let userid = button.dataset.userid;
 
-    const userid = button.dataset.userid;
-
+    // метод DELETE запроса - это переделать
     fetch(`${AppConsts.BaseUrl}/api/v1/users/id=${userid}/delete`, {
       method: 'DELETE'
-
-    })
-      .then(response => response.json())
-      .then(json => console.log(json))
+    }).then(response => response.json())
+      .then(json => console.log(json));
   }
 
-  let elements = document.getElementsByClassName('delete-button-submit');
+  // Свернуть/Развернуть классы спортсменов.
+  function onCollapse(event) {
+    event.preventDefault();
+    console.log('click');
 
-  for (let element of elements) {
-    element.addEventListener('click', onSubmit);
+    this.classList.toggle('active');
+    let content = this.nextElementSibling;
+
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + 'px';
+    }
   }
-})();
 
-updateStatementGGPSubscribing()
-const elements = document.getElementsByClassName('ggp-classes-sub')
+  // Функция подписки на классы в соответствии с логикой.
+  function patchSubGGPClasses(event, operation, literal) {
+    let button = event.target;
+    const userid = button.dataset.userid;
 
-function handleClickFunction(event) {
-  event.target.classList.toggle('active');
-}
-
-for (let element of elements) {
-  element.addEventListener('click', handleClickFunction)
-}
-
-fetch(`${AppConsts.BaseUrl}/api/v1/users`, {
-  method: 'GET',
-  credentials: 'include',
-})
-  .then(response => {
-
-    return response.json()
-  })
-  .then(json => {
-
-    json.data.forEach(user => {
-      let userId = user.id
-      let formUser = document.getElementsByClassName('user');
-      for (let elem of formUser) {                                       // перебор html форм class="user"
-        let formId = elem.getAttribute('data-userid')
-        if (userId == formId) {
-
-          let ggpClasses = elem.getElementsByClassName('ggp-classes-sub');
-          for (let elem of ggpClasses) {
-            console.log(elem.textContent)
-
-
-            user.ggp_sub_classes.forEach(classLetter => {
-              console.log(classLetter.sport_class)
-              if (elem.textContent == classLetter.sport_class) {
-                elem.classList.add('active')
-              }
-            })
-          }
-        }
+    //медот PATCH запроса
+    fetch(`${AppConsts.BaseUrl}/api/v1/users/id=${userid}/subscribe`, {
+      method: 'PATCH',
+      body: JSON.stringify({
+        op: operation,
+        sport_class: literal,
+      }),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8'
       }
+    }).then(response => response.json())
+      .then(json => console.log(json.details));
+  }
+
+  // Переключатель свечения кнопки и выбора метода.
+  function onToggleSubGGpClasses(event) {
+    event.target.classList.toggle('active');
+
+    if (event.target.getAttribute('class').includes('active')) {
+      patchSubGGPClasses(event, 'add', event.target.innerText);
+    } else {
+      patchSubGGPClasses(event, 'remove', event.target.innerText);
+    }
+  }
+
+  // Создаём функцию, в которой объявляем переменные, условие и делаем запрос
+  async function onExit(event) {
+    // отмена действия браузера, чтобы он ничего не делал, пока юзер на нажмёт на кнопку
+    event.preventDefault();
+
+    // объявляем константу с URL-адресом, на котрый будет отправляться запрос
+    const requestLogoutURL = `${AppConsts.BaseUrl}/auth/jwt/logout`;
+
+    // создаётся ф-я запроса методом FETCH, с Асинхронным запросом
+    let response = await fetch(requestLogoutURL, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({})
     });
 
+    // проверка статуса для вывода сообщения о результате выполнения запроса(ошибка или успех)
+    if (response.status == 200 || response.status == 204) {
+      alert('Success');
+      window.location = "login";
+    } else if (response.status == 401) {
+      alert('Inactive user');
+      window.location = "login";
 
-  })
-  .catch(error => console.log(error))
+    } else console.log('Что-то пошло нетак, обратитесь в поддержку');
+  };
 
+  // Добавляем обработчики событий.
+  function addEventListeners() {
+    // Сворачивание кнопок классов
+    let collapseButtons = document.getElementsByClassName('collaps');
+    for (let collapseButton of collapseButtons) {
+      collapseButton.addEventListener('click', onCollapse);
+    }
 
-// delete-post-запрос
+    let deleteButtons = document.getElementsByClassName('delete-button-submit');
+    for (let deleteButton of deleteButtons) {
+      deleteButton.addEventListener('click', onDelete);
+    }
 
-const exit = document.getElementById('patch-button-exit')
-exit.addEventListener('click', onSubmitExit);
+    const toggleButtons = document.getElementsByClassName('ggp-classes-sub')
 
-// объявляем константу с URL-адресом, на котрый будет отправляться запрос
-const requestURL = `${AppConsts.BaseUrl}/auth/jwt/logout`;
+    for (let toggleButton of toggleButtons) {
+      toggleButton.addEventListener('click', onToggleSubGGpClasses);
+    }
 
-// создаём функцию, в которой объявляем переменные, условие и делаем запрос
-async function onSubmitExit(event) {
+    const exitButton = document.getElementById('patch-button-exit');
+    exitButton.addEventListener('click', onExit);
+  }
 
-  //отмена действия браузера, чтобы он ничего не делал, пока юзер на нажмёт на кнопку
-  event.preventDefault();
+  function init() {
+    addEventListeners();
+    getUsers();
+  }
 
-  // создаётся ф-я запроса методом FETCH, с Асинхронным запросом
-  let response = await fetch(requestURL, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json'
-    },
-    body: JSON.stringify({})
-  });
-
-  // проверка статуса для вывода сообщения о результате выполнения запроса(ошибка или успех)
-  if (response.status == 200) {
-    alert('Success')
-    window.location = "login";
-
-  } else if (response.status == 204) {
-    alert('No content')
-    window.location = "login";
-
-  } else if (response.status == 401) {
-
-    alert('Inactive user');
-    window.location = "login";
-
-  } else console.log('Что-то пошло нетак, обратитесь в поддержку');
-
-};
+  // Инициализируем rmg.users.
+  init();
+})();
