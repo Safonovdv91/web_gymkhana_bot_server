4aeaebd525d76c38f9a9fc511cc9c6901db5db15
Safonov <Sofik.d@gmail.com>
2024-03-22 23:56:13 +0300

    add auto-generate app.consts for js

--- src/config.py
+++ src/config.py
@@ -2,12 +2,13 @@ import os
 
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
+SERVER_STATE = os.environ.get("SERVER_STATE")
 LOG_LEVEL = os.environ.get("LOG_LEVEL")
 LOGGER_LOKI_URL = os.environ.get("LOGGER_LOKI_URL")
 
+BASE_URL = os.environ.get("BASE_URL")
 SITE_URL = f"localhost.{os.environ.get('SITE_URL')}"
 SITE_PORT = os.environ.get("SITE_PORT")
 
@@ -23,10 +24,13 @@ DB_NAME_TEST = os.environ.get("DB_NAME_TEST")
 DB_USER_TEST = os.environ.get("DB_USER_TEST")
 DB_PASS_TEST = os.environ.get("DB_PASS_TEST")
 
-
 REDIS_HOST = os.environ.get("REDIS_HOST")
 REDIS_PORT = os.environ.get("REDIS_PORT")
 
 SECRET_AUTH = os.environ.get("SECRET_AUTH")
 
 SECRET_AUTH_MANAGER = os.environ.get("SECRET_AUTH_MANAGER")
+
+# Передача переменной окружения в JavaScript
+with open("src/frontend/static/js/app-const.js", "w") as f:
+    f.write(f"const AppConsts = {{\n'BaseUrl': '{BASE_URL}'\n}} ")
